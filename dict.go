170a0f3d527dbc12cad9e5bf7be40712400ccfc5
DollyDays <2475442190@qq.com>
2024-05-27 01:27:59 +0800

    push

--- dict.go
+++ dict.go
@@ -1,6 +1,9 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"math"
+)
 
 const (
 	INIT_SIZE    int64 = 8
@@ -24,11 +27,12 @@ type Entry struct {
 type htable struct {
 	table []*Entry
 	size  int64
-	mask  int64
+	mask  int64 // 数组长度，算槽位的时候需要用key.hash & mask 算出来
 	used  int64
 }
 
 type DictType struct {
+	// 模板方法
 	HashFunc  func(key *Gobj) int64
 	EqualFunc func(k1, k2 *Gobj) bool
 }
@@ -37,4 +41,119 @@ type Dict struct {
 	DictType
 	hts       [2]*htable
 	rehashidx int64
+	// iterators
+}
+
+func DictCreate(dictType DictType) *Dict {
+	var dict Dict
+	dict.DictType = dictType
+	dict.rehashidx = -1 // 默认为-1 if > -1 则代表 rehashing
+	return &dict
+}
+
+func (dict *Dict) isRehashing() bool {
+	return dict.rehashidx != -1
+}
+
+func (dict *Dict) rehashStep() {
+	// TODO:check iterators
+	dict.rehash(DEFAULT_STEP)
+}
+
+func (dict *Dict) rehash(step int) {
+	for step > 0 {
+		// 当used为0时，代表rehash已经结束，hts[0]的数据已经全部迁移至hts[1]
+		if dict.hts[0].used == 0 {
+			dict.hts[0] = dict.hts[1]
+			dict.hts[1] = nil
+			dict.rehashidx = -1
+			return
+		}
+		// find an nonull slot
+		for dict.hts[0].table[dict.rehashidx] == nil {
+			dict.rehashidx += 1
+		}
+		entry := dict.hts[0].table[dict.rehashidx]
+		// migrate all keys in this slot
+		for entry != nil {
+			ne := entry.next
+			idx := dict.HashFunc(entry.Key) & dict.hts[1].mask
+			entry.next = dict.hts[1].table[idx]
+			dict.hts[1].table[idx] = entry
+			dict.hts[0].used -= 1
+			dict.hts[1].used += 1
+			entry = ne
+		}
+		dict.hts[0].table[dict.rehashidx] = nil
+		dict.rehashidx += 1
+		step -= 1
+	}
+}
+
+func nextPower(size int64) int64 {
+	for i := INIT_SIZE; i < math.MaxInt64; i *= 2 {
+		if i >= size {
+			return i
+		}
+	}
+	return -1
+}
+
+func (dict *Dict) expand(size int64) error {
+	sz := nextPower(size)
+	if dict.isRehashing() || (dict.hts[0] != nil && dict.hts[0].size >= sz) {
+		return EP_ERR
+	}
+	var ht htable
+	ht.size = sz
+	ht.mask = sz - 1
+	ht.table = make([]*Entry, sz)
+	ht.used = 0
+	// check for init
+	if dict.hts[0] == nil {
+		dict.hts[0] = &ht
+		return nil
+	}
+	dict.hts[1] = &ht
+	dict.rehashidx = 0
+	return nil
+}
+
+func (dict *Dict) expandIfNeeded() error {
+	if dict.isRehashing() {
+		return nil
+	}
+	if dict.hts[0] == nil {
+		return dict.expand(INIT_SIZE)
+	}
+	// 判断是否满足扩容因子
+	if (dict.hts[0].used > dict.hts[0].size) && (dict.hts[0].used/dict.hts[0].size > FORCE_RATIO) {
+		return dict.expand(dict.hts[0].size * GROW_RATIO)
+	}
+	return nil
+}
+
+// return the index of a free slot,return -1 if the key is exists or err.
+// 说人话：找一个空槽位给新的元素插进来
+func (dict *Dict) keyIndex(key *Gobj) int64 {
+	err := dict.expandIfNeeded()
+	if err != nil {
+		return -1
+	}
+	h := dict.HashFunc(key)
+	var idx int64
+	for i := 0; i <= 1; i++ {
+		idx = h & dict.hts[i].mask
+		e := dict.hts[i].table[idx]
+		for e != nil {
+			if dict.EqualFunc(e.Key, key) {
+				return -1
+			}
+			e = e.next
+		}
+		if !dict.isRehashing() {
+			break
+		}
+	}
+	return idx
 }
